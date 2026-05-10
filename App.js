beae51855249cb58f297e82a1b54703eb1aa3ce3
augusto9260 <augustoj331@gmail.com>
2026-05-10 15:39:20 -0300

    feat: ajusta layout para o furo da câmera e prepara v1.1

---
+++ App.js
@@ -0,0 +1,182 @@
+import React, { useState, useEffect }  from "react";
+import { View, Text, StyleSheet, StatusBar, FlatList, TextInput, TouchableOpacity, Keyboard, Image, Platform} from "react-native";
+import AsyncStorage  from "@react-native-async-storage/async-storage";
+
+export default function App(){
+  const [tarefas, setTarefas] = useState('');
+  const [listaTarefas, setListaTarefas] = useState([]);
+  
+  //Carregar dados
+  useEffect(() => {
+    carregarDados();
+  },[])
+
+  async function carregarDados(){
+    try {
+      const dados = await AsyncStorage.getItem('@taskmaster_lista');
+      if (dados){
+        setListaTarefas(JSON.parse(dados));
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  //Salvar Dados
+  useEffect(() => {
+    async function salvarDados(){
+      await AsyncStorage.setItem('@taskmaster_lista', JSON.stringify(listaTarefas));
+    }
+    salvarDados();
+  }, [listaTarefas]);
+
+  function addTarefas(){
+    try {
+      if(tarefas.trim() !== ''){
+        const novaLista = {
+          key: Math.random().toString(36).substring(2, 10),
+          tarefas: tarefas,
+          concluida: false,
+        };
+        setListaTarefas([...listaTarefas, novaLista])
+        setTarefas('');
+        Keyboard.dismiss();
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  //Marca a tarefa como finalizada
+  function finalizada(key){
+    try {
+      const novaLista = listaTarefas.map(item => {
+        if(item.key === key){
+          return {...item, concluida: !item.concluida};
+        }
+        return item;
+      })
+      setListaTarefas(novaLista)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  async function excluirTarefa(key){
+    try{
+      const dados = await AsyncStorage.getItem('@taskmaster_lista');
+      const resultado = JSON.parse(dados)// conversão de string para array(vetores)
+
+      const NovaListaTarefas = resultado.filter((item => {
+        return item.key !== key
+      }))
+
+      setListaTarefas(NovaListaTarefas)
+
+      await AsyncStorage.setItem('@taskmaster_lista', JSON.stringify(NovaListaTarefas))
+
+      console.log(NovaListaTarefas)
+    }catch(erro){
+      console.log(erro)
+    }
+  }
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor={"#000000"} barStyle={"light-content"} />
+      <View style={styles.h1}>
+        <Text style={styles.titulo}>TaskMaster</Text>
+      </View>
+      <View style={styles.viewInput}>
+        <TextInput value={tarefas} style={styles.input}
+        onChangeText={setTarefas} />
+        <TouchableOpacity style={styles.botao} 
+        onPress={addTarefas} >
+          <Text style={styles.botaoAdd}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.viewLista}>
+        <FlatList data={listaTarefas} renderItem={({item}) => 
+          <View style={styles.viewBotao}>
+            <Text style={styles.tarefas}>{item.tarefas}</Text>
+            <TouchableOpacity style={styles.botaoLista} 
+            onPress={() => finalizada(item.key)} >
+              <Image style={styles.imageBotao} source={item.concluida && require('./src/assets/marca-de-verificacao.png')}/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => excluirTarefa(item.key)} >
+              <Image source={require('./src/assets/excluir-logo.png')} style={styles.logoExcluir} />
+            </TouchableOpacity>
+          </View>}/>
+      </View>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#007575',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  titulo: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 40,
+  },
+  botaoAdd:{
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  viewInput: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: '#fff',
+    flex:1,
+    height: 48,
+    margin: 5,
+    padding: 8,
+    color: '#fff',
+    borderRadius: 5,
+  },
+  botao:{
+    margin: 4,
+    borderWidth: 1,
+    borderColor: '#fff',
+    padding: 8,
+    borderRadius: 5,
+    width: 48,
+    height: 48,
+  },
+  viewLista:{
+    
+  },
+  viewBotao:{
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 4,
+  },
+  tarefas:{
+    fontSize: 30,
+    color: "#fff",
+    flexShrink: 1,
+    marginLeft: 4,
+  },
+  botaoLista:{
+    width: 32,
+    height: 32,
+    marginLeft: 4,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#009999"
+  },
+  imageBotao: {
+    width: 32,
+    height: 32,
+  },
+  logoExcluir:{
+    width: 32,
+    height: 32,
+  }
+})
